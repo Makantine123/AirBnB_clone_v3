4827c446cd784383f9907d5f8caa5d29f031ec59
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-07-16 11:25:14 +0200

    Commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,5 +20,4 @@ def teardown_appcontext(exception):
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(getenv("HBNB_API_PORT", 5000))
-
     app.run(host=host, port=port, threaded=True)
