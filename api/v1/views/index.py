7f8304c5b6d615409ad464e372bd1ecf2e181953
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-07-16 12:58:00 +0200

    Commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route("/status", methods=[GET])
+@app_views.route("/status", methods=["GET"])
 def get_status():
     """Returns status"""
     return jsonify({"status": "OK"})
