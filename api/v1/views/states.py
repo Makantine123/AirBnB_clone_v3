0a1b61028efd866e06de312d4478ebdfd7089796
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-07-16 14:50:39 +0200

    Commit

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,15 +3,10 @@
 States module app
 """
 
-from flask import jsonify, make_response
+from flask import jsonify, make_response, abort, request
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
 from models.state import State
-from models.user import User
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
