d7df958035070854db7ec00bd5796f13f4ce70eb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-07-16 12:44:10 +0200

    Commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""
-App module
-"""
-from os import getenv
-from flask import Flask
+"""Module"""
+from flask import Flask, jsonify
 from models import storage
+import os
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """Handles app teardown"""
+def teardown_db(self):
+    """Registers a function to be called when the application context ends."""
     storage.close()
 
-
-if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    if os.getenv('HBNB_API_HOST'):
+        bnb_host = os.getenv('HBNB_API_HOST')
+    else:
+        bnb_host = '0.0.0.0'
+    if os.getenv('HBNB_API_PORT'):
+        bnb_port = int(os.getenv('HBNB_API_PORT'))
+    else:
+        bnb_port = 5000
+    app.run(host=bnb_host, port=bnb_port, threaded=True)
