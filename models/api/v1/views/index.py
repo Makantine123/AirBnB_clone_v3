6c165a0a5192e0f0a1db5a95e9f8752bd8faa1f5
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-07-16 11:15:34 +0200

    Commit

---
+++ models/api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Index module app
+"""
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route("/status", methods=[GET])
+def get_status():
+    """Returns status"""
+    return jsonify({"status": "OK"})
