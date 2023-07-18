2769d155eb12cd6b84dd6ccf3e12d413ff0a6e27
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-07-18 10:18:40 +0200

    commit

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from models import storage
 from models.city import City
 from models.place import Place
+from models.user import User
 
 
 @app_views.route("/cities/<city_id>/places", methods=['GET'],
