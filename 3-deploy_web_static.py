1c6430737211ce0500e953484f4107eea1db1b5a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-07-18 11:42:58 +0200

    Commit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['3.84.237.43', '100.25.180.143']
 
 
 def do_pack():
